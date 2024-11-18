63210a9b45b3b3af94a90625ea5ad7bd8e1b2b6e
Max Natanaelsson <max.natanaelsson@outlook.com>
2024-11-18 21:38:05 +0100

    Add project files.

---
+++ MainApp/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hej!");
+Console.WriteLine("Vänligen ange förnamn och efternamn nedan.");
+
+Console.WriteLine("Förnamn: ");
+string firstName = Console.ReadLine()!;
+
+Console.WriteLine("Efternamn: ");
+string lastName = Console.ReadLine()!;
+
+Console.WriteLine("Välkommen " + firstName + " " + lastName + "!");
+Console.ReadKey();
